c7d7fb6a201c882d8cb06d9d36dc024a20a00649
Christopher <127637098+Krisstopher15@users.noreply.github.com>
2023-07-12 04:30:07 -0400

    extend

--- build-car/src/main.rs
+++ build-car/src/main.rs
@@ -1,35 +1,50 @@
-// Declare Car struct to describe vehicle with four named fields
+#[derive(Debug)]
 struct Car {
     color: String,
     transmission: Transmission,
-    convertible: bool,
-    mileage: u32,
+    roof: bool,
+    age: (Age, u32),
 }
 
 
-// Declare enum for Car transmission type
 #[derive(PartialEq, Debug)]
 enum Transmission {
-    // todo!("Fix enum definition so code compiles");
     Manual,
     SemiAuto,
     Automatic,
 }
+#[derive(PartialEq, Debug)]
+enum Age {
+    New,
+    Used,
+}
+
+fn car_quality (miles: u32) -> (Age, u32) {
+    let quality = (Age::New, miles + 0);
+    quality 
+}
 
 
-fn car_factory(color: String, transmission: Transmission, convertible: bool) -> Car {
-    let car = Car {color, transmission, convertible, mileage: 0};
+fn car_factory(color: String, transmission: Transmission, roof: bool, age: (Age, u32)) -> Car {
+    let car = Car {color, transmission, roof, age};
     car
 }
     
 fn main() {
+    let colors: [&str; 4] = ["Blue","Green","Red","Silver"];
 
-        let mut car = car_factory(String::from("Black"), Transmission::Automatic, true);
-    println!("Car 1 = {}, {:?} transmission, convertible {:?}, mileage {:?}", car.color, car.transmission, car.convertible, car.mileage);
+    let mut car: Car = Car {color: String::from(colors[0]), transmission: Transmission::Automatic, roof: false, age: car_quality(0)};
+    println!("{:?}", car);
 
-    car = car_factory(String::from("White"), Transmission::SemiAuto, false);
-    println!("Car 2 = {}, {:?} transmission, convertible {:?}, mileage {:?}", car.color, car.transmission, car.convertible, car.mileage);
+    let mut engine: Transmission = Transmission::Manual;
+    car = car_factory(String::from(colors[1]), engine, false, car_quality(200));
+    println!("Car order 1: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.transmission, car.color, car.age.1);
 
-    car = car_factory(String::from("Blue"), Transmission::Manual, false);
-    println!("Car 3 = {}, {:?} transmission, convertible {:?}, mileage {:?}", car.color, car.transmission, car.convertible, car.mileage)
+    engine = Transmission::SemiAuto;
+    car = car_factory(String::from(colors[2]), engine, false, car_quality(0));
+    println!("Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.transmission, car.color, car.age.1);
+
+    engine = Transmission::Automatic;
+    car = car_factory(String::from(colors[3]), engine, true, car_quality(100));
+    println!("Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.transmission, car.color, car.age.1);
 }
