15a439d54cf17531de735a6edc14b63c520ed9bf
Christopher <127637098+Krisstopher15@users.noreply.github.com>
2023-09-01 01:22:18 -0400

    generics impl and structs

---
+++ impl-generics/src/main.rs
@@ -0,0 +1,47 @@
+#[derive(Debug)]
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {result}");
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {result}");
+
+    let integer = Point {x: 10, y: 20};
+    // let float = Point {x: 2.34, y: 1.53};
+    // let mixed = Point {x: 20, y: 12.34};
+    // dbg!(&integer, &float, &mixed);
+
+    let p = Point {x: 5, y: 10};
+    dbg!(p.mixup(integer));
+
+}
